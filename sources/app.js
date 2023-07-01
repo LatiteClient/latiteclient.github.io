ba1265b584254b87342da926619e813141a59466
Plextora <71889427+Plextora@users.noreply.github.com>
2023-06-30 21:06:18 -0400

    Device check

--- sources/app.js
+++ sources/app.js
@@ -1,16 +1,27 @@
-const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+const accordionItemHeaders = document.querySelectorAll(
+  ".accordion-item-header"
+);
 
-accordionItemHeaders.forEach(accordionItemHeader => {
-  accordionItemHeader.addEventListener("click", event => {
-
+accordionItemHeaders.forEach((accordionItemHeader) => {
+  accordionItemHeader.addEventListener("click", (event) => {
     accordionItemHeader.classList.toggle("active");
     const accordionItemBody = accordionItemHeader.nextElementSibling;
-    if(accordionItemHeader.classList.contains("active")) {
+    if (accordionItemHeader.classList.contains("active")) {
       accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
-    }
-    else {
+    } else {
       accordionItemBody.style.maxHeight = 0;
     }
-    
   });
 });
+
+function isWindows() {
+  if (
+    navigator.userAgent.includes("Windows") === false &&
+    navigator.userAgent.includes("x64") === false &&
+    window.location.href.includes("?bypass-device-check") === false
+  ) {
+    window.location.href = "not-supported.html";
+  }
+}
+
+window.onload = isWindows();
